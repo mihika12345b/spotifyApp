3c5bb179c0438a095ede7f5e030fb81d3a35668c
Mihika Bhatnagar <mihika12345@berkeley.edu>
2023-04-18 02:07:16 -0700

    done

--- app.js
+++ app.js
@@ -3,37 +3,33 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
-// Set up a new Spotify API client
 const spotifyApi = new SpotifyWebApi({
   clientId: 'f305ea7c9bac4e06a36383190ed86422',
   clientSecret: '6d3cdd57b933401d848b02055e31b8c2',
   redirectUri: 'http://localhost:3000/callback'
 });
 
-// Set up a simple HTTP server
 const server = app.listen(3000, () => {
   console.log(`Server running on port ${server.address().port}`);
 });
 
-// Handle requests to the home page
 app.get('/', (req, res) => {
-  // Redirect the user to the Spotify authorization page
+  // for authorizing user
   const authorizeURL = spotifyApi.createAuthorizeURL(['user-top-read']);
   res.redirect(authorizeURL);
 });
 
-// Handle requests to the Spotify callback page
 app.get('/callback', async (req, res) => {
-  // Exchange the authorization code for an access token
+  // gets token from code
   const { code } = req.query;
   const data = await spotifyApi.authorizationCodeGrant(code);
   const { access_token } = data.body;
 
-  // Set the access token on the Spotify API client
+  // sets token
   spotifyApi.setAccessToken(access_token);
 
-  // Get the user's top 10 tracks and render them in the browser
-  const topTracks = await spotifyApi.getMyTopTracks({ limit: 10 });
-  const trackNames = topTracks.body.items.map(track => track.name);
-  res.send(`<h1>Your Top 10 Tracks:</h1><ul>${trackNames.map(name => `<li>${name}</li>`).join('')}</ul>`);
+  // returns top ten songs
+  const topSongs = await spotifyApi.getMyTopTracks({ limit: 10 });
+  const songNames = topSongs.body.items.map(song => song.name);
+  res.send(`<h1>Your Top 10 Tracks:</h1><ul>${songNames.map(name => `<li>${name}</li>`).join('')}</ul>`);
 });
